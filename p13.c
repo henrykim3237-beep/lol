2d6403ef6a33329178807a1edfd29ae61dbc3be1
henrykim3237-beep <henrykim3237@gmail.com>
2026-06-20 15:40:38 +0900

    project 13

---
+++ p13.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+int main(void)
+{
+	printf("Hello C Programming!");
+	return 0;
+}
+int main(void)
+{
+	return 1;
+}
+int main(void)
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d+%d=%d\n", a, b, a + b);
+	printf("%d-%d=%d\n", a, b, a - b);
+	printf("%d*%d=%d\n", a, b, a * b);
+	printf("%d/%d=%d\n", a, b, a / b);
+	return 0;
+}
+int main(void)
+{
+	int a = 7, b = 3;
+	printf("%d, %d", a / b, a%b);
+	return 0;
+}
+int main(void)
+{
+	char f;
+	scanf("%c", &f);
+	printf("%d", f);
+	return 0;
+}*/
+int main(void)
+{
+	int f;
+	scanf("%d", &f);
+	printf("%c", f);
+	return 0;
+}
