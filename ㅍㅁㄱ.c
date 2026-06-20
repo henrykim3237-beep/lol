4600c220515f279ae37bd05a692f35a1f34441f9
henrykim3237-beep <henrykim3237@gmail.com>
2026-06-20 15:20:53 +0900

    project 1-2

---
+++ ㅍㅁㄱ.c
@@ -0,0 +1,15 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main(void)
+
+{
+	int result;
+	int num1, num2, num3;
+
+	printf("세 개의 정수 입력:");
+	scanf("%d %d %d", &num1, &num2, &num3);
+
+	result = num1 + num2 + num3;
+	printf("%d + %d + %d = %d \n", num1, num2, num3, result);
+	return 0;
+}
